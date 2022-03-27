072e953dba121ed42db81ab4f939a6d303a05275
Ilmari  Tarpila <ilmari@tarpila.fi>
2022-03-27 18:00:27 +0300

    Remove browser locale test
    Co-authored-by: HeyImLate <HeyImLate@users.noreply.github.com>

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,19 @@
 import { siteInfo } from "./siteData"
 import { languages } from "./translations"
 
-export const getSupportedBrowserLocale = () => {
-    const browserLocale = window.navigator.language
-    const matchingLocalLang = 
-        browserLocale
-        ? Object.entries(languages).find(
-            ([key, langString])  => browserLocale.includes(langString))
-        : null
-    return matchingLocalLang[1]
-}
+// export const getSupportedBrowserLocale = () => {
+//     const browserLocale = window.navigator.language
+//     const matchingLocalLang = 
+//         browserLocale
+//         ? Object.entries(languages).find(
+//             ([key, langString])  => browserLocale.includes(langString))
+//         : null
+//     return matchingLocalLang[1]
+// }
 
 export const translate = (translation, locale ) => {
     if (!locale) {
-        locale = getSupportedBrowserLocale() || siteInfo.defaultLocale
+        locale = siteInfo.defaultLocale
     }
     return translation[locale] || ""
 }
